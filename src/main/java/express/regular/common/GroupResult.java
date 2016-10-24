e7a9e5d74bb366d13056b7fd69a09a1367685927
bdh92123 <bdh92123.1@gmail.com>
2016-10-24 18:00:04 +0900

    Change GroupResult structure.

--- src/main/java/express/regular/common/GroupResult.java
+++ src/main/java/express/regular/common/GroupResult.java
@@ -1,19 +1,61 @@
 package express.regular.common;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by bdh92123 on 2016-09-26.
  */
-public class GroupResult extends BaseResult<List<String>> {
+public class GroupResult extends BaseResult<GroupResult.GroupsList> {
+    public static class GroupsList {
+        private Map<String, Object> additionalData = new LinkedHashMap<String, Object>();
+        private List<List<String>> list = new ArrayList<List<String>>();
+
+        public GroupsList() {
+        }
+
+        public GroupsList(List<String> firstGroups) {
+            list.add(firstGroups);
+        }
+
+        public List<String> getGroups(int i) {
+            return list.get(i);
+        }
+
+        public void addGroups(List<String> groups) {
+            list.add(groups);
+        }
+
+        public int size() {
+            return list.size();
+        }
+
+        public List<List<String>> getList() {
+            return list;
+        }
+
+        public void setList(List<List<String>> list) {
+            this.list = list;
+        }
+
+        public Map<String, Object> getAdditionalData() {
+            return additionalData;
+        }
+
+        public void setAdditionalData(Map<String, Object> additionalData) {
+            this.additionalData = additionalData;
+        }
+    }
+
     private List<String> columns = new ArrayList<String>();
 
     public GroupResult() {
-        setResultList(new ArrayList<List<String>>());
+        setResultList(new ArrayList<GroupsList>());
     }
 
-    public GroupResult(List<List<String>> data) {
+    public GroupResult(List<GroupsList> data) {
         setResultList(data);
     }
 
@@ -26,12 +68,20 @@ public class GroupResult extends BaseResult<List<String>> {
     }
 
     public String toString() {
-        int i, j;
+        int i, j, k;
         StringBuffer resultBuffer = new StringBuffer();
         for(i=0;i<resultList.size();i++) {
-            List<String> row = resultList.get(i);
-            for(j=0;j<row.size();j++) {
-                resultBuffer.append(row.get(j)).append(" ");
+            GroupsList groupsList = resultList.get(i);
+            if(groupsList != null) {
+                for (j = 0; j < groupsList.size(); j++) {
+                    List<String> groups = groupsList.getGroups(j);
+
+                    for (k = 0; k < groups.size(); k++) {
+                        resultBuffer.append(groups.get(k)).append(" ");
+                    }
+
+                    resultBuffer.append("\n");
+                }
             }
             resultBuffer.append("\n");
         }
--- src/main/java/express/regular/common/StringResult.java
+++ src/main/java/express/regular/common/StringResult.java
@@ -1,15 +1,21 @@
 package express.regular.common;
 
+import java.util.ArrayList;
+
 /**
  * Created by bdh92123 on 2016-09-26.
  */
 public class StringResult extends BaseResult<String> {
 
+    public StringResult() {
+        setResultList(new ArrayList<String>());
+    }
+
     public String toString() {
         int i;
         StringBuffer resultBuffer = new StringBuffer();
         for(i=0;i<resultList.size();i++) {
-            resultBuffer.append(resultList.get(i)).append("\n");
+            resultBuffer.append(String.valueOf(resultList.get(i))).append("\n");
         }
         return resultBuffer.toString();
     }
